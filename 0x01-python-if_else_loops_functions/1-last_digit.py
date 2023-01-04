4d7c2f14bde72f651da8df992c9d0101be4f5f39
nerbrain <malikmox@gmail.com>
2023-01-04 14:09:38 +0000

    line too long edit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,10 @@ else:
     lastInt = number % 10
 
 if lastInt > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, lastInt))
+    print("Last digit of {} is {} and is greater than 5"
+            .format(number, lastInt))
 elif lastInt == 0:
     print("Last digit of {} is {} and is 0".format(number, lastInt))
 else:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, lastInt))
+    print("Last digit of {} is {} and is less than 6 and not 0"
+            .format(number, lastInt))
