81044ab9361951c075f8eeea173b6eb0df6472bd
nerbrain <malikmox@gmail.com>
2023-02-08 11:35:39 +0300

    tasks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""File reading function"""
+"""File-reading function"""
+
 
 def read_file(filename=""):
-
+    """Prints the contents of a UTF8 text file"""
     with open(filename, encoding="utf-8") as f:
-            print(f.read(), end="")
+        print(f.read(), end="")
